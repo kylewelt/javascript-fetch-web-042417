d6bb82ceceaa4abe53783b089ac886ec4f11c195
kylewelt <kylewelt@umich.edu>
2017-06-14 15:15:43 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
-const app = "I don't do much.";
+const token = '47b08cc0251dfe7dc63e7d3793271808a17c5904'
+
+fetch('https://api.github.com/kylewelt/repos', {
+  headers: {
+    Authorization: `token ${token}`
+  }
+}).then(res => res.json()).then(json => console.log(json))
